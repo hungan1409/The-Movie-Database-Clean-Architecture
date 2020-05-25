45ca1dce0e76eab74dce169f7671af7f2f09ed43
nguyen.hung.an <nguyen.hung.an@sun-asterisk.com>
2020-05-25 09:27:31 +0700

    Add load more

--- presentation/src/main/java/com/example/moviedatabase/ui/home/HomeFragment.kt
+++ presentation/src/main/java/com/example/moviedatabase/ui/home/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.moviedatabase.R
 import com.example.moviedatabase.base.BaseFragment
 import com.example.moviedatabase.databinding.FragmentHomeBinding
@@ -46,14 +48,80 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
             moviesRecommendations.observe(viewLifecycleOwner, Observer {
 
             })
+
             viewDataBinding.apply {
                 recyclerPopular.adapter = popularsAdapter
                 recyclerTopRated.adapter = topRatedAdapter
                 recyclerUpcoming.adapter = upcomingAdapter
+
+                swipeRefreshLayout.setOnRefreshListener {
+                    swipeRefreshLayout.isRefreshing = false
+                    onMovieRefreshApi()
+                }
             }
-            getMovieListPopular(1)
-            getMovieListTopRated(1)
-            getMovieListUpcoming(1)
+
+            onMovieRefreshApi()
+            initScrollListener()
+        }
+    }
+
+    private fun onMovieRefreshApi() {
+        viewModel.apply {
+            refreshMoviePage()
+            getMovieListPopular(pagePopulars)
+            getMovieListTopRated(pageTopRated)
+            getMovieListUpcoming(pageUpcoming)
+        }
+    }
+
+    private fun initScrollListener() {
+        viewDataBinding.apply {
+            viewModel?.apply {
+                recyclerPopular.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                        super.onScrolled(recyclerView, dx, dy)
+                        val linearLayoutManager = recyclerView.layoutManager
+                        if (linearLayoutManager is LinearLayoutManager) {
+                            val movieLastItem = (moviesPopulars.value?.size ?: 0) - 1
+                            if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == movieLastItem) {
+                                pagePopulars++
+                                getMovieListPopular(pagePopulars)
+                            }
+                        }
+
+                    }
+                })
+
+                recyclerUpcoming.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                        super.onScrolled(recyclerView, dx, dy)
+                        val linearLayoutManager = recyclerView.layoutManager
+                        if (linearLayoutManager is LinearLayoutManager) {
+                            val movieLastItem = (moviesUpcoming.value?.size ?: 0) - 1
+                            if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == movieLastItem) {
+                                pageUpcoming++
+                                getMovieListUpcoming(pageUpcoming)
+                            }
+                        }
+
+                    }
+                })
+
+                recyclerTopRated.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                        super.onScrolled(recyclerView, dx, dy)
+                        val linearLayoutManager = recyclerView.layoutManager
+                        if (linearLayoutManager is LinearLayoutManager) {
+                            val movieLastItem = (moviesTopRated.value?.size ?: 0) - 1
+                            if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == movieLastItem) {
+                                pageTopRated++
+                                getMovieListTopRated(pageTopRated)
+                            }
+                        }
+
+                    }
+                })
+            }
         }
     }
 }
